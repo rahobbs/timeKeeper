b0920feb475ba48ae03cb62fb39b074c6172d244
Rachael Hobbs <hobbs.rachael@gmail.com>
2015-05-13 12:00:21 -0400

    timestamp button clicks

--- scripts.js
+++ scripts.js
@@ -8,4 +8,13 @@ var newDate = new Date();
 newDate.setDate(newDate.getDate() + 1);    
 $('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
 
+var reply_click = function()
+{ 
+    alert("Button clicked! It's " + jQuery.now() + " milliseconds past Jan 1 1970.");
+}
+document.getElementById('Start').onclick = reply_click;
+document.getElementById('Stop').onclick = reply_click;
+
+
 });
+
